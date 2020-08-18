3deccbccad07aaa69ce3a7af6bd5c0c45a02f1cb
valtera2 <valtera@gmail.com>
2020-08-18 09:33:26 +0000

    fix oracle.sql

--- oracle.sql
+++ oracle.sql
@@ -1,24 +1,25 @@
 --set serveroutput on size 100000
 
 declare
-	match_count integer;
+  match_count integer;
+  v_sql varchar(255);
 begin
 for t in (select ac.owner, ac.table_name, ac.column_name
-	from all_tab_columns ac
+  from all_tab_columns ac
 join all_tables t on ac.table_name = t.table_name and
 ac.owner = t.owner
 where ac.table_name like '%' and ac.owner = user and ac.data_type like
 '%CHAR%'
-	) loop
-
-execute immediate
-	'select count(*) from ' || t.owner || '.' || t.table_name ||
-	'where '||'"' || t.column_name || '"' ||' = :1'
-	into match_count
-	using 'search_string';
-	if match_count > 0 then
-	dbms_output.put_lin( t.table_name ||' '||t.column.name||' '||
-match_count );
-	end if;
-	end loop;
+  ) loop
+  
+  v_sql := 'select count(*) from ' || t.owner || '.' || t.table_name ||
+  ' where '||'"' || t.column_name || '"' ||' = :1';
+  
+  execute immediate v_sql
+  into match_count
+  using 'search_string';
+  if match_count > 0 then
+    dbms_output.put_line( t.table_name || ' ' ||t.column_name|| ' ' || match_count );
+  end if;
+  end loop;
 end;
